79f694cd6fc4ebd80143f74f0919b443d6c93afa
lamhoang1256 <lamhoang1256@gmail.com>
2023-01-27 11:08:20 +0700

    Fix: error response from api at category page

--- next.config.js
+++ next.config.js
@@ -7,7 +7,15 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")]
   },
-  images: { domains: ["images.weserv.nl", "img.rrmj.tv", "img.netpop.app", "img.rr.tv"] }
+  images: { domains: ["images.weserv.nl", "img.rrmj.tv", "img.netpop.app", "img.rr.tv"] },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "https://net-film.vercel.app" }]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
