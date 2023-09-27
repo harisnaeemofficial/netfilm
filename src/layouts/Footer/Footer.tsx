359b19f2cf706e77c46082e92ece0301af861969
Haris Naeem <harrisnaeem991@gmail.com>
2023-09-27 22:39:15 +0100

    Footer.tsx

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -7,11 +7,11 @@ const Footer = () => {
   return (
     <footer>
       <div className={classNames(styles.footer, "container")}>
-        <span>Nguyen Hoang Lam &copy; 2022</span>
+        <span>&copy; 2023</span>
         <div className={styles.footerContact}>
-          <span>Source code: </span>
-          <a target="_blank" rel="noopener noreferrer" href={//PATH.projectGithub}>
-            <IconGithub />
+          <span> </span>
+          <a target="_blank" rel="noopener noreferrer" href={}>
+            
           </a>
         </div>
       </div>
